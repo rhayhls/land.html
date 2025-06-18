e28af99146791bbaeb440870973093e31443bd32
rhayhls <00001117728705sp@al.educacao.sp.gov.br>
2025-06-18 13:54:46 -0300

    Create digianscript.js

---
+++ digianscript.js
@@ -0,0 +1,40 @@
+function irparaSobre() {
+    const imagem = document.getElementById('isobre');
+    const posicaoY = imagem.getBoundingClientRect().top;
+
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+
+}
+function irparaServ() {
+    const imagem = document.getElementById('iserv');
+    const posicaoY = imagem.getBoundingClientRect().top;
+
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+}
+function irparaContato() {
+    const imagem = document.getElementById('icontato');
+    const posicaoY = imagem.getBoundingClientRect().top;
+
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+}
+function irparaInicio() {
+    const imagem = document.getElementById('inicio');
+    const posicaoY = imagem.getBoundingClientRect().top;
+
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+}
